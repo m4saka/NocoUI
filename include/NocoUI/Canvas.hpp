7dd246ae819a42c4d4b01890fd4dbc451a567188
masaka <masaka@mz-kb.com>
2025-02-24 01:41:04 +0900

    Canvasをソースファイルへ分割

--- include/NocoUI/Canvas.hpp
+++ include/NocoUI/Canvas.hpp
@@ -33,194 +33,44 @@ namespace noco
 		Vec2 m_offset = Vec2::Zero();
 		Vec2 m_scale = Vec2::One();
 
-		Mat3x2 rootEffectMat() const
-		{
-			if (m_scale == Vec2::One() && m_offset == Vec2::Zero())
-			{
-				return Mat3x2::Identity();
-			}
-			return Mat3x2::Scale(m_scale) * Mat3x2::Translate(m_offset);
-		}
+		Mat3x2 rootEffectMat() const;
 
-		explicit Canvas()
-			: Canvas{ Node::Create(
-				U"Canvas",
-				AnchorConstraint
-				{
-					.anchorMin = Anchor::TopLeft,
-					.anchorMax = Anchor::BottomRight,
-					.posDelta = Vec2{ 0, 0 },
-					.sizeDelta = Vec2{ 0, 0 },
-					.sizeDeltaPivot = Anchor::MiddleCenter,
-				},
-				IsHitTargetYN::No) }
-		{
-		}
+		Canvas();
 
-		explicit Canvas(const std::shared_ptr<Node>& rootNode)
-			: m_rootNode{ rootNode }
-		{
-			refreshLayout();
-		}
+		explicit Canvas(const std::shared_ptr<Node>& rootNode);
 
 	public:
 		[[nodiscard]]
-		static std::shared_ptr<Canvas> Create()
-		{
-			std::shared_ptr<Canvas> canvas{ new Canvas{} };
-
-			// コンストラクタ内ではshared_from_this()が使えないためここで設定
-			canvas->rootNode()->setCanvasRecursive(canvas);
-
-			return canvas;
-		}
+		static std::shared_ptr<Canvas> Create();
 
 		[[nodiscard]]
-		static std::shared_ptr<Canvas> Create(const std::shared_ptr<Node>& rootNode, RefreshesLayoutYN refreshesLayoutPre = RefreshesLayoutYN::Yes, RefreshesLayoutYN refreshesLayoutPost = RefreshesLayoutYN::Yes)
-		{
-			std::shared_ptr<Canvas> canvas{ new Canvas{ rootNode } };
+		static std::shared_ptr<Canvas> Create(const std::shared_ptr<Node>& rootNode, RefreshesLayoutYN refreshesLayoutPre = RefreshesLayoutYN::Yes, RefreshesLayoutYN refreshesLayoutPost = RefreshesLayoutYN::Yes);
 
-			// rootNodeは同一インスタンスになる想定
-			assert(canvas->rootNode().get() == rootNode.get());
-			
-			rootNode->setCanvasRecursive(canvas); // コンストラクタ内ではshared_from_this()が使えないためここで設定
+		void refreshLayout();
 
-			if (refreshesLayoutPre)
-			{
-				canvas->refreshLayout();
-			}
-			rootNode->resetScrollOffsetRecursive(RefreshesLayoutYN::No);
-			if (refreshesLayoutPost)
-			{
-				canvas->refreshLayout();
-			}
-
-			return canvas;
-		}
-
-		void refreshLayout()
-		{
-			const auto& rootConstraint = m_rootNode->constraint();
-			if (const auto pBoxConstraint = std::get_if<BoxConstraint>(&rootConstraint))
-			{
-				m_rootNode->m_layoutAppliedRect = pBoxConstraint->applyConstraint(Scene::Rect(), Vec2::Zero());
-			}
-			else if (const auto pAnchorConstraint = std::get_if<AnchorConstraint>(&rootConstraint))
-			{
-				m_rootNode->m_layoutAppliedRect = pAnchorConstraint->applyConstraint(Scene::Rect(), Vec2::Zero());
-			}
-			else
-			{
-				// TODO: 実行時例外ではなくコンパイルエラーにしたい
-				throw Error{ U"Unknown root node constraint" };
-			}
-
-			m_rootNode->refreshChildrenLayout();
-			m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
-		}
-
-		bool containsNodeByName(const String& nodeName) const
-		{
-			return m_rootNode->containsChildByName(nodeName, RecursiveYN::Yes);
-		}
+		bool containsNodeByName(const String& nodeName) const;
 
 		[[nodiscard]]
-		std::shared_ptr<Node> getNodeByName(const String& nodeName) const
-		{
-			return m_rootNode->getChildByName(nodeName, RecursiveYN::Yes);
-		}
+		std::shared_ptr<Node> getNodeByName(const String& nodeName) const;
 
 		[[nodiscard]]
-		JSON toJSON() const
-		{
-			return m_rootNode->toJSON();
-		}
+		JSON toJSON() const;
 
 		[[nodiscard]]
-		static std::shared_ptr<Canvas> CreateFromJSON(const JSON& json, RefreshesLayoutYN refreshesLayout = RefreshesLayoutYN::Yes)
-		{
-			return Create(Node::CreateFromJSON(json), refreshesLayout);
-		}
+		static std::shared_ptr<Canvas> CreateFromJSON(const JSON& json, RefreshesLayoutYN refreshesLayout = RefreshesLayoutYN::Yes);
 
-		bool tryReadFromJSON(const JSON& json, RefreshesLayoutYN refreshesLayoutPre = RefreshesLayoutYN::Yes, RefreshesLayoutYN refreshesLayoutPost = RefreshesLayoutYN::Yes)
-		{
-			m_rootNode = Node::CreateFromJSON(json);
-			m_rootNode->setCanvasRecursive(shared_from_this()); // コンストラクタ内ではshared_from_this()が使えないためここで設定
-			if (refreshesLayoutPre)
-			{
-				refreshLayout();
-			}
-			m_rootNode->resetScrollOffsetRecursive(RefreshesLayoutYN::No, RefreshesLayoutYN::No);
-			if (refreshesLayoutPost)
-			{
-				refreshLayout();
-			}
-			return true; // TODO: 失敗したらfalseを返す
-		}
+		bool tryReadFromJSON(const JSON& json, RefreshesLayoutYN refreshesLayoutPre = RefreshesLayoutYN::Yes, RefreshesLayoutYN refreshesLayoutPost = RefreshesLayoutYN::Yes);
 
-		void update(CanvasUpdateContext* pContext = nullptr)
-		{
-			// ホバー中ノード取得
-			const bool canHover = (pContext ? pContext->canHover && !pContext->isHovered() : true) && Window::GetState().focused; // TODO: 本来はウィンドウがアクティブでない場合もホバーさせたい
-			const auto hoveredNode = canHover ? m_rootNode->hoveredNodeInChildren() : nullptr;
+		void update(CanvasUpdateContext* pContext = nullptr);
 
-			// スクロール可能なホバー中ノード取得
-			auto scrollableHoveredNode = hoveredNode ? hoveredNode->findContainedScrollableNode() : nullptr;
-			if (scrollableHoveredNode && !scrollableHoveredNode->rect().mouseOver())
-			{
-				// 子がホバー中でもスクロール可能ノード自身にマウスカーソルが重なっていない場合はスクロールしない
-				scrollableHoveredNode = nullptr;
-			}
-
-			// スクロール実行
-			if (scrollableHoveredNode)
-			{
-				const double wheel = Mouse::Wheel();
-				const double wheelH = Mouse::WheelH();
-				if (wheel != 0.0 || wheelH != 0.0)
-				{
-					scrollableHoveredNode->scroll(Vec2{ wheelH * 50, wheel * 50 });
-				}
-			}
-
-			// ノード更新
-			m_rootNode->update(pContext, hoveredNode, scrollableHoveredNode, Scene::DeltaTime(), rootEffectMat(), m_scale, InteractableYN::Yes, InteractState::Default, InteractState::Default);
-
-			if (pContext)
-			{
-				pContext->canHover = pContext->canHover && hoveredNode == nullptr;
-				if (hoveredNode)
-				{
-					pContext->hoveredNode = hoveredNode;
-				}
-				if (scrollableHoveredNode)
-				{
-					pContext->scrollableHoveredNode = scrollableHoveredNode;
-				}
-			}
-		}
-
-		void draw() const
-		{
-			m_rootNode->draw();
-		}
+		void draw() const;
 
 		[[nodiscard]]
-		const std::shared_ptr<Node>& rootNode() const
-		{
-			return m_rootNode;
-		}
+		const std::shared_ptr<Node>& rootNode() const;
 
-		void removeChildrenAll()
-		{
-			m_rootNode->removeChildrenAll();
-		}
+		void removeChildrenAll();
 
-		void setOffset(const Vec2& offset)
-		{
-			m_offset = offset;
-			m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
-		}
+		void setOffset(const Vec2& offset);
 
 		[[nodiscard]]
 		const Vec2& offset() const
@@ -228,11 +78,7 @@ namespace noco
 			return m_offset;
 		}
 
-		void setScale(const Vec2& scale)
-		{
-			m_scale = scale;
-			m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
-		}
+		void setScale(const Vec2& scale);
 
 		[[nodiscard]]
 		const Vec2& scale() const
@@ -240,20 +86,8 @@ namespace noco
 			return m_scale;
 		}
 
-		void setOffsetScale(const Vec2& offset, const Vec2& scale)
-		{
-			m_offset = offset;
-			m_scale = scale;
-			m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
-		}
+		void setOffsetScale(const Vec2& offset, const Vec2& scale);
 
-		void resetScrollOffsetRecursive(RefreshesLayoutYN refreshesLayout = RefreshesLayoutYN::Yes)
-		{
-			m_rootNode->resetScrollOffsetRecursive(RefreshesLayoutYN::No);
-			if (refreshesLayout)
-			{
-				refreshLayout();
-			}
-		}
+		void resetScrollOffsetRecursive(RefreshesLayoutYN refreshesLayout = RefreshesLayoutYN::Yes);
 	};
 }
---
+++ src/Canvas.cpp
@@ -0,0 +1,209 @@
+﻿#include "NocoUI/Canvas.hpp"
+#include <cassert>
+
+namespace noco
+{
+	Mat3x2 Canvas::rootEffectMat() const
+	{
+		if (m_scale == Vec2::One() && m_offset == Vec2::Zero())
+		{
+			return Mat3x2::Identity();
+		}
+		return Mat3x2::Scale(m_scale) * Mat3x2::Translate(m_offset);
+	}
+
+	Canvas::Canvas()
+		: Canvas{ Node::Create(
+			U"Canvas",
+			AnchorConstraint
+			{
+				.anchorMin = Anchor::TopLeft,
+				.anchorMax = Anchor::BottomRight,
+				.posDelta = Vec2{ 0, 0 },
+				.sizeDelta = Vec2{ 0, 0 },
+				.sizeDeltaPivot = Anchor::MiddleCenter,
+			},
+			IsHitTargetYN::No) }
+	{
+	}
+
+	Canvas::Canvas(const std::shared_ptr<Node>& rootNode)
+		: m_rootNode{ rootNode }
+	{
+		refreshLayout();
+	}
+
+	std::shared_ptr<Canvas> Canvas::Create()
+	{
+		std::shared_ptr<Canvas> canvas{ new Canvas{} };
+
+		// コンストラクタ内ではshared_from_this()が使えないためここで設定
+		canvas->rootNode()->setCanvasRecursive(canvas);
+
+		return canvas;
+	}
+
+	std::shared_ptr<Canvas> Canvas::Create(const std::shared_ptr<Node>& rootNode, RefreshesLayoutYN refreshesLayoutPre, RefreshesLayoutYN refreshesLayoutPost)
+	{
+		std::shared_ptr<Canvas> canvas{ new Canvas{ rootNode } };
+
+		// rootNodeは同一インスタンスになる想定
+		assert(canvas->rootNode().get() == rootNode.get());
+
+		rootNode->setCanvasRecursive(canvas); // コンストラクタ内ではshared_from_this()が使えないためここで設定
+
+		if (refreshesLayoutPre)
+		{
+			canvas->refreshLayout();
+		}
+		rootNode->resetScrollOffsetRecursive(RefreshesLayoutYN::No);
+		if (refreshesLayoutPost)
+		{
+			canvas->refreshLayout();
+		}
+
+		return canvas;
+	}
+
+	void Canvas::refreshLayout()
+	{
+		const auto& rootConstraint = m_rootNode->constraint();
+		if (const auto pBoxConstraint = std::get_if<BoxConstraint>(&rootConstraint))
+		{
+			m_rootNode->m_layoutAppliedRect = pBoxConstraint->applyConstraint(Scene::Rect(), Vec2::Zero());
+		}
+		else if (const auto pAnchorConstraint = std::get_if<AnchorConstraint>(&rootConstraint))
+		{
+			m_rootNode->m_layoutAppliedRect = pAnchorConstraint->applyConstraint(Scene::Rect(), Vec2::Zero());
+		}
+		else
+		{
+			// TODO: 実行時例外ではなくコンパイルエラーにしたい
+			throw Error{ U"Unknown root node constraint" };
+		}
+
+		m_rootNode->refreshChildrenLayout();
+		m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
+	}
+
+	bool Canvas::containsNodeByName(const String& nodeName) const
+	{
+		return m_rootNode->containsChildByName(nodeName, RecursiveYN::Yes);
+	}
+	
+	std::shared_ptr<Node> Canvas::getNodeByName(const String& nodeName) const
+	{
+		return m_rootNode->getChildByName(nodeName, RecursiveYN::Yes);
+	}
+	
+	JSON Canvas::toJSON() const
+	{
+		return m_rootNode->toJSON();
+	}
+	
+	std::shared_ptr<Canvas> Canvas::CreateFromJSON(const JSON& json, RefreshesLayoutYN refreshesLayout)
+	{
+		return Create(Node::CreateFromJSON(json), refreshesLayout);
+	}
+	
+	bool Canvas::tryReadFromJSON(const JSON& json, RefreshesLayoutYN refreshesLayoutPre, RefreshesLayoutYN refreshesLayoutPost)
+	{
+		m_rootNode = Node::CreateFromJSON(json);
+		m_rootNode->setCanvasRecursive(shared_from_this()); // コンストラクタ内ではshared_from_this()が使えないためここで設定
+		if (refreshesLayoutPre)
+		{
+			refreshLayout();
+		}
+		m_rootNode->resetScrollOffsetRecursive(RefreshesLayoutYN::No, RefreshesLayoutYN::No);
+		if (refreshesLayoutPost)
+		{
+			refreshLayout();
+		}
+		return true; // TODO: 失敗したらfalseを返す
+	}
+	
+	void Canvas::update(CanvasUpdateContext* pContext)
+	{
+		// ホバー中ノード取得
+		const bool canHover = (pContext ? pContext->canHover && !pContext->isHovered() : true) && Window::GetState().focused; // TODO: 本来はウィンドウがアクティブでない場合もホバーさせたい
+		const auto hoveredNode = canHover ? m_rootNode->hoveredNodeInChildren() : nullptr;
+
+		// スクロール可能なホバー中ノード取得
+		auto scrollableHoveredNode = hoveredNode ? hoveredNode->findContainedScrollableNode() : nullptr;
+		if (scrollableHoveredNode && !scrollableHoveredNode->rect().mouseOver())
+		{
+			// 子がホバー中でもスクロール可能ノード自身にマウスカーソルが重なっていない場合はスクロールしない
+			scrollableHoveredNode = nullptr;
+		}
+
+		// スクロール実行
+		if (scrollableHoveredNode)
+		{
+			const double wheel = Mouse::Wheel();
+			const double wheelH = Mouse::WheelH();
+			if (wheel != 0.0 || wheelH != 0.0)
+			{
+				scrollableHoveredNode->scroll(Vec2{ wheelH * 50, wheel * 50 });
+			}
+		}
+
+		// ノード更新
+		m_rootNode->update(pContext, hoveredNode, scrollableHoveredNode, Scene::DeltaTime(), rootEffectMat(), m_scale, InteractableYN::Yes, InteractState::Default, InteractState::Default);
+
+		if (pContext)
+		{
+			pContext->canHover = pContext->canHover && hoveredNode == nullptr;
+			if (hoveredNode)
+			{
+				pContext->hoveredNode = hoveredNode;
+			}
+			if (scrollableHoveredNode)
+			{
+				pContext->scrollableHoveredNode = scrollableHoveredNode;
+			}
+		}
+	}
+	
+	void Canvas::draw() const
+	{
+		m_rootNode->draw();
+	}
+	
+	const std::shared_ptr<Node>& Canvas::rootNode() const
+	{
+		return m_rootNode;
+	}
+	
+	void Canvas::removeChildrenAll()
+	{
+		m_rootNode->removeChildrenAll();
+	}
+
+	void Canvas::setOffset(const Vec2& offset)
+	{
+		m_offset = offset;
+		m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
+	}
+	
+	void Canvas::setScale(const Vec2& scale)
+	{
+		m_scale = scale;
+		m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
+	}
+	
+	void Canvas::setOffsetScale(const Vec2& offset, const Vec2& scale)
+	{
+		m_offset = offset;
+		m_scale = scale;
+		m_rootNode->refreshEffectedRect(rootEffectMat(), m_scale);
+	}
+	
+	void Canvas::resetScrollOffsetRecursive(RefreshesLayoutYN refreshesLayout)
+	{
+		m_rootNode->resetScrollOffsetRecursive(RefreshesLayoutYN::No);
+		if (refreshesLayout)
+		{
+			refreshLayout();
+		}
+	}
+}
