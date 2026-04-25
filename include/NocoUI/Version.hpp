0a48fe3c6a77389ce42795c417dd40f18255eff8
masaka <masaka@mz-kb.com>
2026-04-25 09:22:07 +0900

    バージョンを0.4.1に上げる

--- include/NocoUI/Version.hpp
+++ include/NocoUI/Version.hpp
@@ -3,9 +3,9 @@
 
 namespace noco
 {
-	constexpr StringView NocoUIVersion = U"0.4.0";
+	constexpr StringView NocoUIVersion = U"0.4.1";
 
-	constexpr int32 CurrentSerializedVersion = 8;
+	constexpr int32 CurrentSerializedVersion = 9;
 
 	/// @brief Number型を廃止してInt/Double型を導入したserializedVersion
 	constexpr int32 SerializedVersion_IntDoubleParamType = 8;
